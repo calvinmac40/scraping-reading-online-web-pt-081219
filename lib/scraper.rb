fc3a04e4b294c35fbae3d6b1150102eaea5336df
Calvin McDowell <calvinmac40@hotmail.com>
2019-10-12 15:15:04 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,8 +6,8 @@ html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 p doc.css(".headline-26OIBN").text
 
-html = open(https://www.craigslist.org/about/sites#US)
+# html = open(https://www.craigslist.org/about/sites#US)
 
-doc = Nokogiri::HTML(open(https://www.craigslist.org/about/sites#US))
-p doc.css("h4").text
+# doc = Nokogiri::HTML(open(https://www.craigslist.org/about/sites#US))
+# p doc.css("h4").text
 
